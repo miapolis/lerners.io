dbc2386b05cb4af881296f53933a036f0bef394d
Miapolis <41233357+Miapolis@users.noreply.github.com>
2021-07-13 13:15:20 -0400

    reverse icon order

--- modules/home/profile-area.tsx
+++ modules/home/profile-area.tsx
@@ -21,11 +21,14 @@ const ProfileArea: React.FC = () => {
   const calcProfileScale = () => {
     const width = window.innerWidth;
     setProfileScale(Math.min(1, width / 600));
-  }
+  };
 
   return (
     <div className={styles.centralContainer}>
-      <div className={styles.centralGroup} style={{transform: `scale(${profileScale})`}}>
+      <div
+        className={styles.centralGroup}
+        style={{ transform: `scale(${profileScale})` }}
+      >
         <img
           className={styles.profileCenter}
           src="https://avatars.githubusercontent.com/u/41233357?s=200"
@@ -37,7 +40,7 @@ const ProfileArea: React.FC = () => {
           src="/images/github.svg"
           alt="github"
           url="https://github.com/miapolis"
-          angle={30}
+          angle={150}
           accent="#6e5494"
           fadeInTime="0.3"
           initialized={init}
@@ -46,7 +49,7 @@ const ProfileArea: React.FC = () => {
           src="/images/discord.svg"
           alt="discord"
           url="https://discord.com/users/508420859476836364"
-          angle={60}
+          angle={120}
           accent="#5865F2"
           fadeInTime="0.4"
           initialized={init}
@@ -64,7 +67,7 @@ const ProfileArea: React.FC = () => {
           src="/images/xbox.svg"
           alt="xbox"
           url="http://live.xbox.com/en-US/Profile?gamertag=Miapolis"
-          angle={120}
+          angle={60}
           accent="#107C10"
           fadeInTime="0.6"
           initialized={init}
@@ -73,7 +76,7 @@ const ProfileArea: React.FC = () => {
           src="/images/steam.svg"
           alt="steam"
           url="https://steamcommunity.com/profiles/76561199072450059"
-          angle={150}
+          angle={30}
           accent="#2a475e"
           fadeInTime="0.7"
           initialized={init}
