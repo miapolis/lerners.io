982fd5122621057d2d6699bf721d5b1624105c08
Ethan Lerner <41233357+miapolis@users.noreply.github.com>
2021-10-13 11:04:33 -0400

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Miapolis</title>
+        <title>Ethan Lerner</title>
         <meta
           name="description"
           content="Yet another personal website on the internet."
