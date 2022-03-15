95a07a0dc5b418585f1db05aa0d5a6362d7264d9
Ethan Lerner <ethan@lerners.io>
2022-03-15 10:03:01 -0400

    fix toggle button

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,18 +33,17 @@ const Home: NextPage = () => {
           } 0)`,
           backgroundSize: "40px 40px",
         }}
+        className="relative"
       >
-        <IconButton size="large" className="absolute top-4 right-4 z-10">
+        <IconButton
+          size="large"
+          className="absolute top-4 right-4 z-10"
+          onClick={toggleTheme}
+        >
           {theme.value == "dark" ? (
-            <DarkModeOutlinedIcon
-              style={{ color: "white" }}
-              onClick={toggleTheme}
-            />
+            <DarkModeOutlinedIcon style={{ color: "white" }} />
           ) : (
-            <LightModeOutlinedIcon
-              style={{ color: "black" }}
-              onClick={toggleTheme}
-            />
+            <LightModeOutlinedIcon style={{ color: "black" }} />
           )}
         </IconButton>
         <FirstSection />
