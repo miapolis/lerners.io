010bd55a878178ade18c698fc622501826e83717
Ethan Lerner <ethan@lerners.io>
2022-11-01 11:16:42 -0400

    blog page, add to navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar: React.FC = () => {
 
   return (
     <header className="flex items-center justify-between py-4 flex-initial gap-4">
-      <Link to="/" className="h-8">
+      <Link to="/" className="h-8 w-8">
         <Themed
           dark={
             <LogoIcon
@@ -23,6 +23,11 @@ export const Navbar: React.FC = () => {
           light={<LogoIcon width={32} height={32} />}
         />
       </Link>
+      <div className="flex-1 mx-4 gap-6 flex">
+        <MainLink href="/about" name="About" />
+        <MainLink href="/blog" name="Blog" />
+        <MainLink href="https://github.com/miapolis" name="GitHub" />
+      </div>
       <button
         onClick={() => {
           setTheme((prev) => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK));
@@ -31,5 +36,19 @@ export const Navbar: React.FC = () => {
         <Themed dark={<IconMoon />} light={<IconSun />} />
       </button>
     </header>
+  );
+};
+
+const MainLink: React.FC<{ href: string; name: string }> = ({ href, name }) => {
+  const Comp = href.startsWith("http") ? "a" : Link;
+
+  return (
+    <Comp
+      to={href}
+      href={href}
+      className="transition-all text-base font-semibold hover:text-indigo-700 dark:hover:text-yellow-400"
+    >
+      {name}
+    </Comp>
   );
 };
