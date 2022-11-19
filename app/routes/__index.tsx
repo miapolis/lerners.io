8350506c6b730a4f9dce3bacdeaf07f06ab3f680
Ethan Lerner <ethan@lerners.io>
2022-11-18 20:58:44 -0500

    default og

--- app/routes/__index.tsx
+++ app/routes/__index.tsx
@@ -1,6 +1,7 @@
-import { LoaderFunction } from "@remix-run/node";
+import { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import { LayoutWrapper } from "~/components/layout-wrapper";
+import { baseUrl } from "~/utils/site-url";
 import { getThemeSession } from "~/utils/theme.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -8,6 +9,27 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   return {
     theme: getTheme(),
+    url: baseUrl(),
+  };
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  const desc =
+    "Hi, I'm Ethan. I'm a student and full-stack software developer interested in frontend design, distributed systems, and game engines.";
+
+  return {
+    title: "Ethan Lerner",
+    description: desc,
+    "og:title": "Ethan Lerner",
+    "og:type": "website",
+    "og:description": desc,
+    "og:image": `${data.url}/og-default.png`,
+
+    "twitter:card": "summary_large_image",
+    "twitter:url": data.url,
+    "twitter:title": "Ethan Lerner",
+    "twitter:description": desc,
+    "twitter:image": `${data.url}/og-default.png`,
   };
 };
 
